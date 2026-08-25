a8281512cd0be96584bcd975557a8671648f80bd
Kr1lm1 <136977752+Kr1lm1@users.noreply.github.com>
2026-08-25 20:38:16 +0300

    Submenu: rotate background 2.5° counter-clockwise

--- MacKards/PieMenuController.swift
+++ MacKards/PieMenuController.swift
@@ -132,7 +132,7 @@ final class PieMenuController {
         let dir = -90.0 + stepDeg * Double(mainApps.count + mainActions.count + groupIndex) + stepDeg / 2
         let iconStart = dir - iconSpanDeg / 2
         let iconArc = iconStart...(iconStart + iconSpanDeg)
-        let bgStart = iconStart - 2.5
+        let bgStart = iconStart - 5.0
         let bgArc = bgStart...(bgStart + bgSpanDeg)
 
         let win = NSWindow(contentRect: frame, styleMask: .borderless, backing: .buffered, defer: false)
