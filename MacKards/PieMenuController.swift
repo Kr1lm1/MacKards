f4e2dd8d9a22825e240f27ceb0294f466d264be4
Kr1lm1 <136977752+Kr1lm1@users.noreply.github.com>
2026-08-25 20:02:02 +0300

    Submenu: shorter arc, rotate 5° more clockwise

--- MacKards/PieMenuController.swift
+++ MacKards/PieMenuController.swift
@@ -120,8 +120,8 @@ final class PieMenuController {
         let minRadius = s.radius + s.ringThickness + 8
         let gapAng = gapSub / Double(minRadius) * 180 / .pi
         let iconAng = mainCard / Double(minRadius) * 180 / .pi
-        let iconSpanDeg = max(30.0, min(200.0, Double(n + 1) * gapAng + Double(n) * iconAng + 20.0))
-        let bgSpanDeg = min(240.0, iconSpanDeg + 20.0)
+        let iconSpanDeg = max(30.0, min(180.0, Double(n + 1) * gapAng + Double(n) * iconAng + 10.0))
+        let bgSpanDeg = min(220.0, iconSpanDeg + 10.0)
         let arcLenAvailable = 2 * .pi * Double(minRadius) * (iconSpanDeg / 360.0)
         let cardSub = max(24.0, min(mainCard, (arcLenAvailable - Double(n + 1) * gapSub) / Double(n)))
         let radius = minRadius
@@ -133,7 +133,7 @@ final class PieMenuController {
         let dir = -90.0 + stepDeg * Double(mainApps.count + mainActions.count + groupIndex) + stepDeg / 2
         let iconStart = dir - iconSpanDeg / 2
         let iconArc = iconStart...(iconStart + iconSpanDeg)
-        let bgStart = iconStart - 25.0
+        let bgStart = iconStart - 20.0
         let bgArc = bgStart...(bgStart + bgSpanDeg)
 
         let win = NSWindow(contentRect: frame, styleMask: .borderless, backing: .buffered, defer: false)
