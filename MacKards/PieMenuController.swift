6de1822a7f1500a970c7363eaeed1260f4244e89
Kr1lm1 <136977752+Kr1lm1@users.noreply.github.com>
2026-08-25 13:02:17 +0300

    Submenu: fix opposite side bug

--- MacKards/PieMenuController.swift
+++ MacKards/PieMenuController.swift
@@ -131,8 +131,7 @@ final class PieMenuController {
         let frame = NSRect(x: menuCenter.x - side/2, y: menuCenter.y - side/2, width: side, height: side)
 
         let stepDeg = 360.0 / Double(max(totalMain, 1))
-        let startA = -90.0 - stepDeg * Double(max(totalMain, 1) - 1) / 2
-        let dir = startA + stepDeg * Double(mainApps.count + mainActions.count + groupIndex)
+        let dir = -90.0 + stepDeg * Double(mainApps.count + mainActions.count + groupIndex) + stepDeg / 2
         let start = dir - spanDeg / 2
         let arcRange = start...(start + spanDeg)
 
