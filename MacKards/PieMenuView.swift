5662f44b4ff3429cb177f22dbd5caf5666841bf4
Kr1lm1 <136977752+Kr1lm1@users.noreply.github.com>
2026-08-25 21:30:09 +0300

    Submenu: remove background arc outline

--- MacKards/PieMenuView.swift
+++ MacKards/PieMenuView.swift
@@ -96,9 +96,6 @@ struct PieMenuContentView: View {
                 let bgThick = max(26, thick * 0.85)
                 ZStack {
                     ArcShape(radius: radius, thickness: bgThick, start: bgRange.lowerBound, end: bgRange.upperBound)
-                        .stroke(outline, style: StrokeStyle(lineWidth: bgThick + 3, lineCap: .butt))
-                        .frame(width: size, height: size)
-                    ArcShape(radius: radius, thickness: bgThick, start: bgRange.lowerBound, end: bgRange.upperBound)
                         .stroke(lp ? AnyShapeStyle(lpColor) : AnyShapeStyle(settings.menuMaterial), style: StrokeStyle(lineWidth: bgThick, lineCap: .butt))
                         .frame(width: size, height: size)
                     let n = Double(max(apps.count, 1))
